553c813c1be6717b95278c9ec32f007f7ace74e3
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-26 00:03:09 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,5 @@
 class AddGradeAndBirthdate < ActiveRecord::Migration[5.1]
   def change
-    add_column :
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :integer
 end
